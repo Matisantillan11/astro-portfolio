48242dda369abaa07c1ca1c0d28a776c5c5575d5
matisantillan-dev <matias@renaiss.ai>
2024-01-18 17:37:06 -0300

    fix: replace server by static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel()
-});
+	integrations: [tailwind(), react()],
+	output: 'static',
+	adapter: vercel(),
+})
