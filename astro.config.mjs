f790b186f81dffb7d543a8aedbc3fa37853f7f56
matisantillan-dev <matias@renaiss.ai>
2024-01-18 17:48:56 -0300

    fix: add hybrid strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	output: 'static',
+	output: 'hybrid',
 	adapter: vercel(),
 })
