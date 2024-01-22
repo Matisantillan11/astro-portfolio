c0e41daa987cd82b1655851ece02b2f02124e06f
matisantillan-dev <matias@renaiss.ai>
2024-01-22 16:50:59 -0500

    fix: env

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,9 @@
-const API_BASE = '/api'
+const ENV = {
+	DEVELOPMENT: 'http://localhost:5173/api',
+	PRODUCTION: 'https://matisantillan.dev/api',
+}
+
+const API_BASE = ENV[import.meta.env.NODE_ENV]
 
 export const API_ROUTE = {
 	NEWSLETTER: {
